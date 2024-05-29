04e7306da6f78b7074fc9b2ae9fe8a137d62c203
bea816 <kimnadon@naver.com>
2024-05-29 18:39:31 +0900

    다대일, 다대다

--- Blog/lionblog/blog/models.py
+++ Blog/lionblog/blog/models.py
@@ -1,9 +1,35 @@
 from django.db import models
+from users.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to = User, on_delete=models.CASCADE, related_name="posts")
+    category = models.ManyToManyField(to=Category, through="PostCategory", related_name="posts")
+    like = models.ManyToManyField(to=User, through="Like", related_name="liked_posts")
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="post_likes")
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="user_likes")
+class PostCategory(models.Model):
+    category = models.ForeignKey(to=Category, on_delete=models.PROTECT, related_name="categories_postcategory")
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="posts_postcategory")
+
+class Comment(models.Model):
+    post = models.ForeignKey(to = Post, on_delete = models.CASCADE, related_name = "comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = "comments")
+
+    def __str__(self):
+        return f'[{self.id}] {self.content}'
--- Blog/lionblog/blog/urls.py
+++ Blog/lionblog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import list, create, detail, update, delete
+from .views import *
 
 app_name = 'blog'
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('detail/<int:id>', detail, name = "detail"),
     path('update/<int:id>', update, name = "update"),
     path('delete/<int:id>', delete, name = "delete"),
+    path('create-comment/<int:post_id>/', create_comment, name = "create-comment"),
+    path('add-like/<int:post_id>/', add_like, name = "add-like"),
+    path('remove-like/<int:post_id>/', remove_like, name = "remove-like"),
+    path('my-like/', mylike, name="my-like"),
 ]
